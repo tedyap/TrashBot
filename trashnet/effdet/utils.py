89ca1d698e2bdc5e6dbdece0419f94d2292a65b6
jsaurabh <j.saur95@gmail.com>
2020-06-08 23:44:07 -0400

    added Swish activation function

---
+++ trashnet/effdet/utils.py
@@ -0,0 +1,13 @@
+"""
+"""
+
+import torch
+import torch.nn as nn
+
+class Swish(nn.Module):
+    """
+    """
+    def forward(self, x):
+        """
+        """
+        return x * torch.sigmoid(x)
