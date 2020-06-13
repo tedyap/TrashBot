d989ac8ed2fbf16efdff14de0ec03ec2df22285e
jsaurabh <j.saur95@gmail.com>
2020-06-13 06:01:51 -0400

    print statements for debugging

--- trashnet/train.py
+++ trashnet/train.py
@@ -27,7 +27,7 @@ def argument_parser(epilog: str = None):
 
     parser = argparse.ArgumentParser(epilog=epilog or f"""
     Example:
-        python train.py --meta /path/to/meta.json --annotations /path/to/annotatios/folder --output /path/to/output.json # noqa: F541
+        python train.py --num_epochs NUM_EPOCHS --valid_interval 1 --es_min_delta MINI_DELTA --es_patience PATIENCE --path /path/to/dat/folder/root --log /path/to/log directory --save_path models # noqa: F541
     """)
 
     parser.add_argument("--image_size", type=int, default=512, help="The height and width for images passed to the network")
@@ -36,7 +36,7 @@ def argument_parser(epilog: str = None):
     parser.add_argument("--alpha", type=float, default=0.25)
     parser.add_argument("--gamma", type=float, default=1.5)
     parser.add_argument("--num_epochs", "-n", type=int, default=100)
-    parser.add_argument("--test_interval", type=int, default=1, help="Number of epochs between testing")
+    parser.add_argument("--valid_interval", type=int, default=1, help="Number of epochs between testing")
     parser.add_argument("--es_min_delta", type=float, default=0.0, help="Early stopping: Minimum change in loss to qualify as improvement")
     parser.add_argument("--es_patience", type=float, default=0.0, help="Number of epochs without improvement to stop training")
     parser.add_argument("--path", "-p", type=str, help="Path to root folder of data in MS-COCO format")
@@ -62,15 +62,15 @@ def train(args):
         'shuffle': True,
         'drop_last': True,
         'collate_fn': collate,
-        'num_workers': 4
+        'num_workers': 10
     }
 
-    test_dict = {
+    valid_dict = {
         'batch_size': args.batch_size,
         'shuffle': False,
         'drop_last': False,
         'collate_fn': collate,
-        'num_workers': 4
+        'num_workers': 10
     }
 
     train_ds = Coco(root=args.path, data="train", transforms=transforms.Compose(
@@ -78,10 +78,10 @@ def train(args):
     ))
     train_dl = DataLoader(train_ds, **train_dict)
 
-    test_ds = Coco(root=args.path, data="valid", transforms=transforms.Compose(
+    valid_ds = Coco(root=args.path, data="valid", transforms=transforms.Compose(
         [Normalize(), Resize()]
     ))
-    test_dl = DataLoader(test_ds, **test_dict)
+    valid_dl = DataLoader(valid_ds, **valid_dict)
 
     model = EfficientDet(n_classes=train_ds.get_num_classes())
 
@@ -121,7 +121,9 @@ def train(args):
                 else:
                     classification_loss, regression_loss = model([data['img'].float(), data['annot']])
                 
-                classification_loss, regression_loss = classification_loss.mean(), regression_loss.mean()
+                classification_loss = classification_loss.mean()
+                regression_loss = regression_loss.mean()
+                
                 loss = classification_loss + regression_loss
                 if loss == 0:
                     continue
@@ -134,9 +136,16 @@ def train(args):
                 losses.append(float(loss))
                 total_loss = np.mean(losses)
 
-                writer.add_scalar('Train/Total Loss', total_loss, epoch * iters_per_epoch + iteration)
-                writer.add_scalar('Train/Regression Loss', regression_loss, epoch * iters_per_epoch + iteration)
-                writer.add_scalar('Train/Focal Loss', classification_loss, epoch * iters_per_epoch + iteration)
+                prog_bar.set_description(
+                    "Epoch: {}/{} Iteration: {}/{} Cls_Loss: {1.5f} Batch_Loss: {1.5f} Total Loss: {1.5f}".format(
+                        epoch + 1, args.num_epochs, iteration + 1, iters_per_epoch, 
+                        classification_loss, regression_loss, loss, total_loss
+                    )
+                    
+                )
+                writer.add_scalar("Train/Total Loss", total_loss, epoch * iters_per_epoch + iteration)
+                writer.add_scalar("Train/Regression Loss", regression_loss, epoch * iters_per_epoch + iteration)
+                writer.add_scalar("Train/Focal Loss", classification_loss, epoch * iters_per_epoch + iteration)
             
             except Exception:
                 logger.error(Exception)
@@ -144,18 +153,19 @@ def train(args):
         
         schedule.step(np.mean(losses))
 
-        if epoch % args.test_interval == 0:
+        if epoch % args.valid_interval == 0:
             model.eval()
             regression_losses, classification_losses = [], []
 
-            for iteration, data in enumerate(test_dl):
+            for iteration, data in enumerate(valid_dl):
                 with torch.no_grad():
                     if torch.cuda.is_available():
                         classification_loss, regression_loss = model([data['img'].cuda().float(), data['annot'].cuda()])
                     else:
                         classification_loss, regression_loss = model([data['img'].float(), data['annot']])
                     
-                    classification_loss, regression_loss = classification_loss.mean(), regression_loss.mean()
+                    classification_loss = classification_loss.mean()
+                    regression_loss = regression_loss.mean()
 
                     classification_losses.append(float(classification_loss))
                     regression_losses.append(float(regression_loss))
@@ -169,7 +179,7 @@ def train(args):
                 "Epoch: {}/{} Classification Loss: {:1.5f} Regression Loss{:1.5f} Total Loss: {:1.5f}".format(
                     epoch + 1, args.num_epochs, cls_loss, reg_loss, np.mean(loss)))
 
-            writer.add_scalar('Val/Total_loss', loss, epoch)
+            writer.add_scalar('Val/Total Loss', loss, epoch)
             writer.add_scalar('Val/Regression Loss', reg_loss, epoch)
             writer.add_scalar('Val/Classfication Loss (focal loss)', cls_loss, epoch)
 
@@ -179,7 +189,7 @@ def train(args):
                 torch.save(model, os.path.join(args.save_path, "trashnet_" + str(epoch) + ".pth"))
 
             if epoch - best_epoch > args.es_patience > 0:
-                logger.debug("Stop training at epoch {}. The lowest loss achieved is {}".format(epoch, loss))
+                logger.debug("Stopping training at epoch {}.  Loss is {}".format(epoch, loss))
                 break
 
     writer.close()
