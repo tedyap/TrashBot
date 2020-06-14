33360ed8a919011a4e0ad61120bbb4c495b8710a
jsaurabh <j.saur95@gmail.com>
2020-06-14 15:41:01 -0400

    general improvements and save feature

--- data/augment_data.py
+++ data/augment_data.py
@@ -2,11 +2,12 @@
 """
 
 import os
+import shutil
 import argparse
+import logging
 import json
 import numpy as np
 import cv2 as cv
-import numpy as np
 import matplotlib.pyplot as plt
 
 from collections import defaultdict
@@ -19,34 +20,35 @@ from augmentation.rotate import RandomRotate
 from augmentation.utils import draw_rectangle
 
 def main():
+    logger = logging.getLogger('logger')
     parser = argument_parser()
     args = parser.parse_args()
 
     jsonfile = args.json
     datapath = args.data
-    # outputdir = args.output
+    outputdir = args.output
 
     with open(jsonfile) as fp:
         data = json.load(fp)
     
-    cat2idx, idx2cat = {}, {}
-    idx2image, image2idx = {}, {}
+    # cat2idx, idx2cat = {}, {}
+    # idx2image, image2idx = {}, {}
     image2annotations = defaultdict(list)
     filenames, paths = [], []
 
     images = data['images']
-    categories = data['categories']
+    # categories = data['categories']
     annotations = data['annotations']
 
     for image in images:
-        idx2image[image['id']] = image['file_name']
-        image2idx[image['file_name']] = image['id']
+        # idx2image[image['id']] = image['file_name']
+        # image2idx[image['file_name']] = image['id']
         filename = image['file_name'].split('/')[-1]
         filenames.append(filename)
 
-    for category in categories:
-        idx2cat[category['id']] = category['name']
-        cat2idx[category['name']] = category['id']
+    # for category in categories:
+    #     idx2cat[category['id']] = category['name']
+    #     cat2idx[category['name']] = category['id']
 
     for annotation in annotations:
         image_id = annotation['image_id']
@@ -59,22 +61,45 @@ def main():
         path = os.path.join(image_folder, filename)
         paths.append(path)
 
-    for idx in range(3):
-        augment_image(paths[idx], np.array(image2annotations[idx]))
+    if not os.path.isdir(outputdir):
+        os.makedirs(outputdir)
 
-def augment_image(path, bbox):
+    image_output = os.path.join(outputdir, 'images')
+    annotations_output = os.path.join(outputdir, 'annotations')
+
+    if os.path.isdir(image_output):
+        shutil.rmtree(image_output)
+    os.makedirs(image_output)
+
+    if os.path.isdir(annotations_output):
+        shutil.rmtree(annotations_output)
+    os.makedirs(annotations_output)
+
+    for idx in range(len(paths)):
+        image, bbox = augment_image(paths[idx], np.array(image2annotations[idx]))
+        image = image[:, :, ::-1]
+        name = 'augmented_' + filenames[idx]
+        augmented_image = os.path.join(image_output, name)
+        try:
+            cv.imwrite(augmented_image, image)
+        except Exception as e:
+            logger.info(e)
+            continue
+
+def augment_image(path, bbox, viz: bool = False):
     image = cv.imread(path)[:, :, ::-1]
-    # bbox = np.array([[402, 0, 799, 270, 1]]).astype('float64')
-    print(bbox, path)
-    plt.imshow(draw_rectangle(image, bbox))
-    plt.show()
+    if viz:
+        plt.imshow(draw_rectangle(image, bbox))
+        plt.show()
 
     transforms = Pipeline([RandomHorizontalFlip(0.7), Scale(0.2, 0.2), RandomRotate((2, 2))])
-    image, bbox = transforms(image, bbox)
+    new_image, new_bbox = transforms(image, bbox)
     
-    print(bbox, path)
-    plt.imshow(draw_rectangle(image, bbox))
-    plt.show()
+    if viz:
+        plt.imshow(draw_rectangle(image, bbox))
+        plt.show()
+    
+    return new_image, new_bbox
 
 def argument_parser(epilog: str = None) -> argparse.ArgumentParser:
     """
